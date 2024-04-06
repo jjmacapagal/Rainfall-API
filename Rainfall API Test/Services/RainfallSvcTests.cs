e72689a54fadaaea5a7d11ee069048110e3beed4
Jorell John Macapagal <macapagal.jorelljohn12@gmail.com>
2024-04-06 20:18:14 +0800

    Add Rainfall API Test to solution

---
+++ Rainfall API Test/Services/RainfallSvcTests.cs
@@ -0,0 +1,62 @@
+using Castle.Core.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Newtonsoft.Json;
+using Rainfall_API.Exceptions.Rainfall;
+using Rainfall_API.Interfaces;
+using Rainfall_API.Services;
+using System.Net;
+
+namespace Rainfall_API_Test.Services
+{
+    [TestClass]
+    public class RainfallSvcTests
+    {
+        [TestMethod]
+        public async Task GetReadings_UnsuccessfulResponse_ThrowsException()
+        {
+            // Arrange
+            var httpClientWrapperMock = new Mock<IHttpClientWrapper>();
+            httpClientWrapperMock.Setup(wrapper => wrapper.GetAsync(It.IsAny<string>()))
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.BadRequest
+                });
+
+            var rainfallSvc = new RainfallSvc(httpClientWrapperMock.Object);
+
+            var exception = await Assert.ThrowsExceptionAsync<InvalidStationIdException>(async () =>
+            {
+                // Call the method that you expect to throw the exception
+                await rainfallSvc.GetReadings("stationId");
+            });
+
+            Assert.IsNotNull(exception);
+        }
+
+        [TestMethod]
+        public async Task GetReadings_SuccessfulResponse_ReturnsReadings()
+        {
+            // Arrange
+            var httpClientWrapperMock = new Mock<IHttpClientWrapper>();
+            httpClientWrapperMock.Setup(wrapper => wrapper.GetAsync(It.IsAny<string>()))
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("{\"items\":[{\"dateTime\":\"2024-04-06T12:00:00\",\"value\":10.5}]}")
+                });
+
+            var rainfallSvc = new RainfallSvc(httpClientWrapperMock.Object);
+
+            // Act
+            var actualReadings = await rainfallSvc.GetReadings("stationId");
+
+            // Assert
+            Assert.IsNotNull(actualReadings);
+            Assert.IsTrue(actualReadings.Count == 1); // Assuming only one reading is returned in the mocked response
+            var result = actualReadings[0];
+            Assert.AreEqual(new DateTime(2024, 04, 06, 12, 00, 00), result.DateMeasured);
+            Assert.AreEqual(10.5, result.AmountMeasured);
+        }
+    }
+}
