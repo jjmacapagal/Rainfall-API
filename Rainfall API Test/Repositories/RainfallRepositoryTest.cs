49b0103189b25a9c0b7e22db127d1b9ea62d0e5c
Jorell John Macapagal <macapagal.jorelljohn12@gmail.com>
2024-04-07 16:33:46 +0800

    refactor rain repo test

--- Rainfall API Test/Repositories/RainfallRepositoryTest.cs
+++ Rainfall API Test/Repositories/RainfallRepositoryTest.cs
@@ -1,6 +1,7 @@
 using Moq;
 using Rainfall_API.Exceptions.Rainfall;
 using Rainfall_API.Interfaces;
+using Rainfall_API.Models.API;
 using Rainfall_API.Repositories;
 using System.Net;
 
@@ -35,11 +36,16 @@ namespace Rainfall_API_Test.Repositories
         {
             // Arrange
             var httpClientWrapperMock = new Mock<IHttpClientWrapper>();
-            httpClientWrapperMock.Setup(wrapper => wrapper.GetAsync(It.IsAny<string>()))
+            var expectedDateMeasured = DateTime.Parse("2024-04-06T12:00:00");
+            var expectedDateMeasuredString = expectedDateMeasured.ToString("yyyy-MM-ddTHH:mm:ss");
+            var expectedAmoundMeasured = 10.5;
+            var content = new StringContent($"{{\"items\":[{{\"dateTime\":\"{expectedDateMeasuredString}\",\"value\":{expectedAmoundMeasured}}}]}}");
+            httpClientWrapperMock
+                .Setup(wrapper => wrapper.GetAsync(It.IsAny<string>()))
                 .ReturnsAsync(new HttpResponseMessage
                 {
                     StatusCode = HttpStatusCode.OK,
-                    Content = new StringContent("{\"items\":[{\"dateTime\":\"2024-04-06T12:00:00\",\"value\":10.5}]}")
+                    Content = content
                 });
 
             var repo = new RainfallRepository(httpClientWrapperMock.Object);
@@ -51,8 +57,8 @@ namespace Rainfall_API_Test.Repositories
             Assert.IsNotNull(result);
             Assert.IsTrue(result.Count == 1); // Assuming only one reading is returned in the mocked response
             var reading = result[0];
-            Assert.AreEqual(new DateTime(2024, 04, 06, 12, 00, 00), reading.DateMeasured);
-            Assert.AreEqual(10.5, reading.AmountMeasured);
+            Assert.AreEqual(expectedDateMeasured, reading.DateMeasured);
+            Assert.AreEqual(expectedAmoundMeasured, reading.AmountMeasured);
         }
     }
 }
